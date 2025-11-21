860a977cbb90eed85a25f485cae38cd33c644a18
Brooklyn <markmclean2005@gmail.com>
2025-11-21 12:32:56 +0200

    added function to fetch intial task and added catch error

--- scripts/api/fetchTasks.js
+++ scripts/api/fetchTasks.js
@@ -1,3 +1,24 @@
 const API_URL = "https://jsl-kanban-api.vercel.app/"
 
 let initialTasks;
+
+
+export async function fetchIntialTasks() {
+  if (initialTasks) {
+    return initialTasks;
+  }
+
+    try {
+        const response = await fetch(`${API_URL}tasks`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        initialTasks = await response.json();
+        return initialTasks;
+    } catch (error) {
+        console.error("Failed to fetch tasks:", error);
+        return [];
+    }
+}
+
+export const allTasks = [];
